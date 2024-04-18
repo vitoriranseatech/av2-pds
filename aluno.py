68c5661aa5941cf1eaede6d7b24ceba41765fa96
vitoriranseatech <137505189+vitoriranseatech@users.noreply.github.com>
2024-04-18 09:58:17 -0300

    ;

---
+++ aluno.py
@@ -0,0 +1,12 @@
+class aluno:
+    def __init__(self,nome,idade, sexo,RA, CPF, RG, nota1, nota2):
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+        self.RA = RA
+        self.CPF = CPF
+        self.RG = RG
+        self.nota1 = nota1
+        self.nota2 = nota2
+
+aluno = ("vitoria","17","feminino","9","999.999.999.","1111111","100","100")    
